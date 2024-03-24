80050417f5c8c717d05e2b745657765d215b55ad
Egor Smirnov <e.smirnov@corp.vk.com>
2024-03-24 17:06:16 +0300

    add workbox

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,29 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env': process.env,
-  },
-  server: {
-    host: true,
-  },
-  base: './',
+    build: {
+        modulePreload: false,
+    },
+    plugins: [
+        react(),
+        VitePWA({
+            devOptions: { enabled: true },
+            registerType: 'autoUpdate',
+            injectRegister: 'inline',
+            strategies: 'generateSW',
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+            },
+        }),
+    ],
+    define: {
+        'process.env': process.env,
+    },
+    server: {
+        host: true,
+    },
+    base: './',
 });
